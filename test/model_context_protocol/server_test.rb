a169bbd2ba446a45a873b465d99a112cf4a98444
Koichi ITO <koic.ito@gmail.com>
2025-05-28 01:11:07 +0900

    Enable RuboCop Minitest and RuboCop Rake for development
    Based on the following information, `rubocop-minitest` and `rubocop-rake` have been added
    for development. Autocorrection has been applied for offenses detected by `rubocop-minitest`.
    
    ```console
    $ bundle exec rake
    (snip)
    
    Running RuboCop...
    Inspecting 40 files
    ........................................
    
    40 files inspected, no offenses detected
    
    Tip: Based on detected gems, the following RuboCop extension libraries might be helpful:
    * rubocop-minitest (https://rubygems.org/gems/rubocop-minitest)
    * rubocop-rake (https://rubygems.org/gems/rubocop-rake)
    
    You can opt out of this message by adding the following to your config
    (see https://docs.rubocop.org/rubocop/extensions.html#extension-suggestions for more options):
    AllCops:
        SuggestExtensions: false
    ```
    
    If preserving the existing behavior is preferred, it is also possible to set `SuggestExtensions: false`.

--- test/model_context_protocol/server_test.rb
+++ test/model_context_protocol/server_test.rb
@@ -153,7 +153,7 @@ module ModelContextProtocol
       assert_kind_of Array, result[:tools]
       assert_equal "test_tool", result[:tools][0][:name]
       assert_equal "Test tool", result[:tools][0][:description]
-      assert_equal({}, result[:tools][0][:inputSchema])
+      assert_empty(result[:tools][0][:inputSchema])
       assert_instrumentation_data({ method: "tools/list" })
     end
 
