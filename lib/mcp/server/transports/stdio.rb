3c8ff5a547509d9b0d86b31d0eeec5593a7b3c3e
Joey Cardosi <joey.cardosi@shopify.com>
2025-05-30 12:17:09 -0400

    fix + cleanup stdio

--- lib/mcp/server/transports/stdio.rb
+++ lib/mcp/server/transports/stdio.rb
@@ -4,31 +4,33 @@ require_relative "../../shared/transport"
 require "json"
 
 module MCP
-  module Transports
-    class StdioTransport < Transport
-      def initialize(server)
-        @server = server
-        @open = false
-        $stdin.set_encoding("UTF-8")
-        $stdout.set_encoding("UTF-8")
-        super
-      end
-
-      def open
-        @open = true
-        while @open && (line = $stdin.gets)
-          handle_json_request(line.strip)
+  class Server
+    module Transports
+      class StdioTransport < Transport
+        def initialize(server)
+          @server = server
+          @open = false
+          $stdin.set_encoding("UTF-8")
+          $stdout.set_encoding("UTF-8")
+          super
         end
-      end
 
-      def close
-        @open = false
-      end
+        def open
+          @open = true
+          while @open && (line = $stdin.gets)
+            handle_json_request(line.strip)
+          end
+        end
 
-      def send_response(message)
-        json_message = message.is_a?(String) ? message : JSON.generate(message)
-        $stdout.puts(json_message)
-        $stdout.flush
+        def close
+          @open = false
+        end
+
+        def send_response(message)
+          json_message = message.is_a?(String) ? message : JSON.generate(message)
+          $stdout.puts(json_message)
+          $stdout.flush
+        end
       end
     end
   end
